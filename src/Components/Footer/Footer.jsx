b0156d149a1b3e999393262a586bcf65118aee27
Jacobo Cohello <118378780+Jcooob@users.noreply.github.com>
2023-05-10 20:32:12 -0500

    fix footer

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,7 +1,4 @@
 import "./Footer.modules.css"
-import facebook from "../../Assets/SocialMediaIcons/facebook.png"
-import twitter from "../../Assets/SocialMediaIcons/twitter.png"
-import instagram from "../../Assets/SocialMediaIcons/instagram.png"
 import github from "../../Assets/SocialMediaIcons/github.png"
 import linkedin from "../../Assets/SocialMediaIcons/linkedin.png"
 
@@ -14,10 +11,8 @@ const Footer = () => {
                 <div className="footer-wrapper">
 
                     <div className="developer-info">
-                        <p className="developer">Designed by Rejcob</p>
+                        <p className="developer">Designed and developed by Rejcob</p>
                         <div className="social-icons">
-                            <a href="https://twitter.com/Jacobo44819835"><img src={twitter} alt="twitterLogo" className="socialMediaIcon" /></a>
-                            <a href="https://www.instagram.com/rejcob/"><img src={instagram} alt="instagramLogo" className="socialMediaIcon" /></a>
                             <a href="https://github.com/Jcooob"><img src={github} alt="githubLogo" className="socialMediaIcon" /></a>
                             <a href="https://www.linkedin.com/in/jacobo-renato-cohello-revolledo-096102251"><img src={linkedin} alt="linkedin" className="socialMediaIcon" /></a>
                         </div>
